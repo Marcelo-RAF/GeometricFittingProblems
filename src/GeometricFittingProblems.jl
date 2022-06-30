d1a6a12289038ae2c75dd71416bd414fd34b04e4
Marcelo <marceloraferreira@gmail.com>
2022-06-30 16:22:50 -0300

    descrição do commit

--- src/GeometricFittingProblems.jl
+++ src/GeometricFittingProblems.jl
@@ -2,7 +2,7 @@ module GeometricFittingProblems
 
 using DelimitedFiles, LinearAlgebra
 
-export load_problem, solve, build_problem, inverse_power_method 
+export load_problem, solve, build_problem, inverse_power_method
 
 import Base.show
 
@@ -14,11 +14,11 @@ It is an immutable type used by main functions of this package
 """
 struct FitProbType
     name::String
-    data:: Array{Float64,2}
-    npts:: Int
-    nout:: Int
+    data::Array{Float64,2}
+    npts::Int
+    nout::Int
     model::Function
-    dim:: Int
+    dim::Int
     cluster::Bool
     noise::Bool
     solution::Array{Float64,1}
@@ -28,9 +28,9 @@ end
 struct FitOutputType
     status::Bool
     solution::Vector{Float64}
-    niter :: Int
-    minimum :: Float64
-    feval :: Int
+    niter::Int
+    minimum::Float64
+    feval::Int
 end
 
 """
@@ -47,51 +47,83 @@ returns a FitProbType
 ```
 """
 function load_problem(filename::String)
-    prob_matrix = readdlm(filename,':')
-    return FitProbType(prob_matrix[1,2],eval(Meta.parse(prob_matrix[2,2])),prob_matrix[3,2],prob_matrix[4,2],eval(Meta.parse(prob_matrix[5,2])),prob_matrix[6,2],prob_matrix[7,2],prob_matrix[8,2],eval(Meta.parse(prob_matrix[9,2])),prob_matrix[10,2])
+    prob_matrix = readdlm(filename, ':')
+    return FitProbType(prob_matrix[1, 2], eval(Meta.parse(prob_matrix[2, 2])), prob_matrix[3, 2], prob_matrix[4, 2], eval(Meta.parse(prob_matrix[5, 2])), prob_matrix[6, 2], prob_matrix[7, 2], prob_matrix[8, 2], eval(Meta.parse(prob_matrix[9, 2])), prob_matrix[10, 2])
 end
 
 
-function solve(prob::FitProbType,θinit::Vector{Float64},method::String)
+function solve(prob::FitProbType, θinit::Vector{Float64}, method::String)
 
 end
 
-function build_problem(probtype::String,limit::Vector{Float64},params::Vector{Float64})
+function build_problem(probtype::String, limit::Vector{Float64}, params::Vector{Float64})
     if probtype == "sphere2D"
         println("params need to be setup as [center,radious,npts,nout]")
-        c = [params[1],params[2]]
+        c = [params[1], params[2]]
         r = params[3]
         npts = Int(params[4])
         x = zeros(npts)
         y = zeros(npts)
         θ = [0.0:2*π/(npts-1):2*π;]
-        for k=1:npts
-            x[k] = c[1]+r*cos(θ[k])
-            y[k] = c[2]+r*sin(θ[k])
+        for k = 1:npts
+            x[k] = c[1] + r * cos(θ[k])
+            y[k] = c[2] + r * sin(θ[k])
         end
         nout = Int(params[5])
         k = 1
         iout = []
-        while k<=nout
+        while k <= nout
             i = rand([1:npts;])
             if i ∉ iout
-                push!(iout,i)
-                k = k+1
+                push!(iout, i)
+                k = k + 1
             end
         end
         for k = 1:nout
-            x[iout[k]]=x[iout[k]]+rand([0.25*r:0.1*(r);(1+0.25)*r])
-            y[iout[k]]=y[iout[k]]+rand([0.25*r:0.1*(r);(1+0.25)*r])
+            x[iout[k]] = x[iout[k]] + rand([0.25*r:0.1*(r); (1 + 0.25) * r])
+            y[iout[k]] = y[iout[k]] + rand([0.25*r:0.1*(r); (1 + 0.25) * r])
         end
-        FileMatrix = ["name :" "sphere2D";"data :" [[x y]]; "npts :" npts;"nout :" nout; "model :" "(x,t) -> (x[1]-t[1])^2 - (x[2]-t[2])^2 - t[3]^2";"dim :" 3; "cluster :" "false"; "noise :" "false"; "solution :" [push!(c,r)]; "description :" "none"]
-        
+        FileMatrix = ["name :" "sphere2D"; "data :" [[x y]]; "npts :" npts; "nout :" nout; "model :" "(x,t) -> (x[1]-t[1])^2 - (x[2]-t[2])^2 - t[3]^2"; "dim :" 3; "cluster :" "false"; "noise :" "false"; "solution :" [push!(c, r)]; "description :" "none"]
+
         open("sphere2D_$(c[1])_$(c[2])_$(c[3])_$(nout).csv", "w") do io
-           writedlm(io, FileMatrix)
+            writedlm(io, FileMatrix)
         end
 
     end
     if probtype == "sphere3D"
-    
+        println("params need to be setup as [center,radious,npts,nout]")
+        c = [params[1], params[2], params[5]]
+        r = params[4]
+        npts = Int(params[5])
+        x = zeros(npts)
+        y = zeros(npts)
+        z = zeros(npts)
+        θ = [0.0:2*π/(npts-1):2*π;]
+        φ = [0.0:π/(npts-1):π;]
+        for k = 1:npts #forma de espiral - ao criar outro forma, se obtem metade dos circulos máximos
+            x[k] = c[1] + r * cos(θ[k]) * sin(φ[k])
+            y[k] = c[2] + r * sin(θ[k]) * sin(φ[k])
+            z[k] = c[3] + r * cos(φ[k])
+        end
+        nout = Int(params[6])
+        k = 1
+        iout = []
+        while k <= nout
+            i = rand([1:npts;])
+            if i ∉ iout
+                push!(iout, i)
+                k = k + 1
+            end
+        end
+        for k = 1:nout
+            x[iout[k]] = x[iout[k]] + rand([0.25*r:0.1*(r); (1 + 0.25) * r])
+            y[iout[k]] = y[iout[k]] + rand([0.25*r:0.1*(r); (1 + 0.25) * r])
+            z[iout[k]] = z[iout[k]] + rand([0.25*r:0.1*(r); (1 + 0.25) * r])
+        end
+        FileMatrix = ["name :" "sphere3D"; "data :" [[x y z]]; "npts :" npts; "nout :" nout; "model :" "(x,t) -> (x[1]-t[1])^2 - (x[2]-t[2])^2 -(x[3]-t[3])^2 - t[4]^2"; "dim :" 4; "cluster :" "false"; "noise :" "false"; "solution :" [push!(c, r)]; "description :" "none"]
+
+        open("sphere3D_$(c[1])_$(c[2])_$(c[3])_$(nout).csv", "w") do io #o que essa linha faz exatamente?
+            writedlm(io, FileMatrix)
     end
 end
 
@@ -111,39 +143,39 @@ julia> inverse_power_method(A,[1.0,1.0,1.0])
 returns ???
 ```
 """
-function inverse_power_method(A::Array{Float64};q0=ones(size(A)[1]),ε=10.0^(-4),limit=100)
+function inverse_power_method(A::Array{Float64}; q0=ones(size(A)[1]), ε=10.0^(-4), limit=100)
     stop_criteria = 1000.0
     F = lu(A)
     B = inv(A)
     k = 1
     s = 0.0
     q = zeros(length(q0))
-    while stop_criteria > ε && k<limit
-        s = norm(q0,Inf)
-        q = B*(q0/s)
-        stop_criteria = norm(abs.(q)-abs.(q0),Inf)
+    while stop_criteria > ε && k < limit
+        s = norm(q0, Inf)
+        q = B * (q0 / s)
+        stop_criteria = norm(abs.(q) - abs.(q0), Inf)
         q0 = copy(q)
-        k = k+1
+        k = k + 1
     end
-    if k==limit
+    if k == limit
         error("iteration limit of inverse power method was reached")
     else
-        return q,1.0/s
+        return q, 1.0 / s
     end
 end
 
 function show(io::IO, fout::FitOutputType)
 
-    print(io,"  ▶ Output ◀ \n")
-    if Bool(fout.status) ==true
-        print(io,"  ↳ Status (.status) = Convergent \n")
+    print(io, "  ▶ Output ◀ \n")
+    if Bool(fout.status) == true
+        print(io, "  ↳ Status (.status) = Convergent \n")
     else
-        print(io,"  ↳ Status (.status) = Divergent \n")
+        print(io, "  ↳ Status (.status) = Divergent \n")
     end
-    print(io,"  ↳ Solution (.solution) = $(fout.solution) \n")
-    print(io,"  ↳ Number of iterations (.niter) = $(fout.niter) \n")
-    print(io,"  ↳ Minimum (.minimum) = $(fout.minimum) \n")
-    print(io,"  ↳ Number of function calls (.feval) = $(fout.feval) \n")
+    print(io, "  ↳ Solution (.solution) = $(fout.solution) \n")
+    print(io, "  ↳ Number of iterations (.niter) = $(fout.niter) \n")
+    print(io, "  ↳ Minimum (.minimum) = $(fout.minimum) \n")
+    print(io, "  ↳ Number of function calls (.feval) = $(fout.feval) \n")
 end
 
 
